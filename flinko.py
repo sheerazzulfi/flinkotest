e7c930ecbe87fb9c8b2f69b4ac28076cf81cd6a9
sheerazzulfi <97657986+sheerazzulfi@users.noreply.github.com>
2022-07-01 13:19:19 +0530

    Update flinko.py

--- flinko.py
+++ flinko.py
@@ -3,49 +3,46 @@ from aifc import Error
 
 import requests
 import json
+import sys
+
 
 class Test_Failed(Error):
     pass
 
-def login(mail, password):
+token = sys.argv[1]
+
+def login(token):
     s = requests.Session()
-    payload = {
-        'emailId': mail,
-        'password': password,
-    }
-    logurl = 'https://app.flinko.com:8101/optimize/v1/public/user/signin'
     head = {
         "accept": "application/json",
         "Content-Type": "application/json"
     }
-    resp = requests.post(logurl, json=payload)
-    tk = json.loads(resp.content)
-    token = tk['responseObject']['access_token']
     head["Authorization"] = "Bearer " + token
-
     suiteid = 'SUITE1003'
     pes = s.post('https://app.flinko.com:8109/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head)
     out = json.loads(pes.content)
     exid = out['responseObject']['id']
 
-    time.sleep(2)
+    time.sleep(3)
     sc = 0
-    fr1 = 0
     while (sc < 1):
-        r1 = s.get('https://app.flinko.com:8110/optimize/v1/executionResponse/result/' + exid, headers=head)
+        r1 = s.get('https://app.flinko.com:8109/optimize/v1/dashboard/execution/' + exid, headers=head)
         c1 = json.loads(r1.content)
-        try:
-            fr2 = c1['responseObject']['suiteStatus']
-            fr1 = fr2
-        except:
-            print("pending....")
-        if fr1 == 'PASS':
-            print('Success')
-            sc = 1
-        if fr1 == 'FAIL':
-            # print("FAILED")
-            raise Test_Failed
-            sc = 1
+        fr1 = c1['responseObject']['executionStatus']
+        print('status : ' + fr1 + '......')
+        if (fr1 == "Completed" or fr1 == "Terminated" or fr1 == "Warning"):
+            if fr1 == 'Completed':
+                r2 = s.get('https://app.flinko.com:8110/optimize/v1/executionResponse/result/' + exid, headers=head)
+                c2 = json.loads(r2.content)
+                fr2 = c2['responseObject']['suiteStatus']
+                if fr2 == 'FAIL':
+                    raise Test_Failed
+                else:
+                    print("End Result : " + "Test Passed")
+                sc = 1
+            elif (fr1 == "Terminated" or fr1 == 'Warning'):
+                print("End Result : " + fr1)
+                sc = 1
         time.sleep(10)
 
-login('sheerazzulfi123@gmail.com', 'Ali5171*')
+login(token)
